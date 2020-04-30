508e4227659c2d568b09989958c72ab416442073
seryte <382643558@qq.com>
2020-04-30 15:18:52 +0000

    add chapter_two

---
+++ Chapter_Two/src/first.c
@@ -0,0 +1,11 @@
+# include <stdio.h> // 预处理指令，包含另一个文件
+int main(void) //总是第一个被调用的函数
+{
+	int num;
+	num = 1;
+	printf("I am a simple ");
+	printf("computer.\n");
+	printf("My favorite number is %d because it is firtst.\n", num);
+
+	return 0;
+}
